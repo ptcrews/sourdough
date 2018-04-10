88a038c4cf354e69ff22fbe9c4a87093c298eaef
Paul Crews <ptcrews@stanford.edu>
2018-04-10 15:48:51 -0700

    Script redirects all output

--- datagrump/vary_window_size.sh
+++ datagrump/vary_window_size.sh
@@ -9,7 +9,7 @@ for count in {1..20};
 do
   WINDOW_SIZE_VAL=$(($count*10))
   echo "Test $count with window size $WINDOW_SIZE_VAL\n"
-  WINDOW_SIZE=$WINDOW_SIZE_VAL ./run-contest fixed_window &>> varied_windown_results.txt;
+  WINDOW_SIZE=$WINDOW_SIZE_VAL ./run-contest fixed_window >> varied_windown_results.txt 2>&1;
 
 done
 
