d734a57e02004f09f5db119982a4e2a29867c2a0
Paul Crews <ptcrews@stanford.edu>
2018-04-10 15:50:47 -0700

    Now removes file before running script

--- datagrump/vary_window_size.sh
+++ datagrump/vary_window_size.sh
@@ -2,14 +2,14 @@
 
 clear
 
-
-file=controller.cc
+OUT_FILE=varied_windown_results.txt
+rm $OUT_FILE
 
 for count in {1..20};
 do
   WINDOW_SIZE_VAL=$(($count*10))
   echo "Test $count with window size $WINDOW_SIZE_VAL\n"
-  WINDOW_SIZE=$WINDOW_SIZE_VAL ./run-contest fixed_window >> varied_windown_results.txt 2>&1;
+  WINDOW_SIZE=$WINDOW_SIZE_VAL ./run-contest fixed_window >> $OUT_FILE 2>&1;
 
 done
 
