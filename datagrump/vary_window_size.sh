b6a44374c497ac0b81147c19070e7c3cd41d30c8
hudson-ayers <hayers@stanford.edu>
2018-04-10 12:40:48 -0700

    pushed changes

--- datagrump/vary_window_size.sh
+++ datagrump/vary_window_size.sh
@@ -5,12 +5,9 @@ clear
 
 file=controller.cc
 
-window_size=1
 for count in {1..20}
 do
-window_size = count*5
-sed -i 's/the_window_size = *;/the_window_size = '$window_size';/' $file
-#./run-contest fixed_window 2>> fixed_windown_repeat.txt
+    WINDOW_SIZE={$COUNT*10} ./run-contest fixed_window 2>> varied_windown_results.txt
 
 done
 
