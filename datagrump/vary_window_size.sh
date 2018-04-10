c54e5955528d4ad00e8bca8182d5a6452ca09860
Paul Crews <ptcrews@stanford.edu>
2018-04-10 15:38:53 -0700

    Fixed vary_window_size script

--- datagrump/vary_window_size.sh
+++ datagrump/vary_window_size.sh
@@ -5,9 +5,11 @@ clear
 
 file=controller.cc
 
-for count in {1..20}
+for count in {1..20};
 do
-    WINDOW_SIZE={$COUNT*10} ./run-contest fixed_window 2>> varied_windown_results.txt
+  WINDOW_SIZE_VAL=$(($count*10))
+  echo "Test $count with window size $WINDOW_SIZE_VAL\n"
+  WINDOW_SIZE=$WINDOW_SIZE_VAL ./run-contest fixed_window 2>> varied_windown_results.txt;
 
 done
 
