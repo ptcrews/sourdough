addc858857466f569a7751fed125ea55faf2b25b
Paul Crews <ptcrews@stanford.edu>
2018-04-10 15:51:47 -0700

    Fixed echo error

--- datagrump/vary_window_size.sh
+++ datagrump/vary_window_size.sh
@@ -8,7 +8,7 @@ rm $OUT_FILE
 for count in {1..20};
 do
   WINDOW_SIZE_VAL=$(($count*10))
-  echo "Test $count with window size $WINDOW_SIZE_VAL\n"
+  echo "Test $count with window size $WINDOW_SIZE_VAL" >> $OUT_FILE
   WINDOW_SIZE=$WINDOW_SIZE_VAL ./run-contest fixed_window >> $OUT_FILE 2>&1;
 
 done
