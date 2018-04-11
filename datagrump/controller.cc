b6c5d7ab0d889e48a91eab0f4cfa6066459ff597
Paul Crews <ptcrews@stanford.edu>
2018-04-11 14:03:07 -0700

    Fixed timeout constant back to default

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -100,5 +100,5 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
    before sending one more datagram */
 unsigned int Controller::timeout_ms()
 {
-  return 500; /* timeout of one second */
+  return 1000; /* timeout of one second */
 }
